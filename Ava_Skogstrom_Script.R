9701882941b4f288db90ace2f1fd6372b9ea9ab5
skogstra <skogstra@bc.edu>
2023-09-01 11:24:28 -0400

    Module 1 Project

---
+++ Ava_Skogstrom_Script.R
@@ -0,0 +1,61 @@
+library(ggplot2)
+library(dplyr)
+
+setwd("~/BIOL3140/Module-1-Project/Scales")
+
+dat <- read.csv("scales.csv")
+dim(dat)
+head(dat)
+
+class(dat$N)
+class(dat$quadrant)
+class(dat$species)
+class(dat$specimen)
+
+sapply(dat,class)
+
+dat$species <- as.factor(dat$species)
+species <- levels(dat$species)
+species 
+length(species)
+
+dat$species==species[1]
+dat$species[dat$species==species[1]]
+
+A.rup <- length(dat$species[dat$species==species[1]])
+L.gib <- length(dat$species[dat$species==species[2]])
+L.mac <- length(dat$species[dat$species==species[3]])
+M.sal <- length(dat$species[dat$species==species[4]])
+M.sax <- length(dat$species[dat$species==species[5]])
+
+P.fla <- length(dat$species[dat$species==species[6]])
+species.obs <- data.frame(sp=species,n=c(A.rup,L.gib,L.mac,M.sal,M.sax,P.fla))
+species.obs
+
+species.n <- dat %>% 
+  group_by(species) %>% 
+  summarise(n=n())
+species.n  
+
+dat %>% 
+  count(species,specimen) %>% 
+  print() %>% 
+  count(species,name="n.specimens")
+
+for(i in species){
+  p <- dat %>% 
+    filter(species==i) %>% 
+    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
+    print(p)
+}
+
+pdf("species_quadrant.pdf")
+for(i in species){
+    p <- dat %>% 
+    filter(species==i) %>% 
+  ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
+  print(p)
+}
+dev.off()
+
+list.files(pattern=".pdf")
