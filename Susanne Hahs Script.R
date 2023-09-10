a3ea5a67615004f2ca802fa38c2217df3da4a8d6
Susanne-hahs <143426989+Susanne-hahs@users.noreply.github.com>
2023-09-10 11:44:26 -0400

    Susanne Hahs Module 1

--- Susanne Hahs Script.R
+++ Susanne Hahs Script.R
@@ -8,35 +8,43 @@ class(dat$N)
 class(dat$quadrant)
 class(dat$species)
 class(dat$specimen)
+
+#Code that reports class of each column in dataset
 sapply(dat,class)
+
 dat$species <- as.factor(dat$species)
 species <- levels(dat$species)
 species
-length(species)
+
+#Code that reports dimensions of dataset 
+print(paste(length(dat), "x", length(species)))
+
 dat$species==species[1]
 dat$species[dat$species==species[1]]
+
 A.rup<-length(dat$species[dat$species==species[1]])
 L.gib<-length(dat$species[dat$species==species[2]])
 L.mac<-length(dat$species[dat$species==species[3]])
 M.sal<-length(dat$species[dat$species==species[4]])
 M.sax<-length(dat$species[dat$species==species[5]])
+
 P.fla<-length(dat$species[dat$species==species[6]])
 species.obs <- data.frame(sp=species,n=c(A.rup,L.gib,L.mac,M.sal,M.sax,P.fla))
 species.obs
-dat %>%
-  group_by(species) %>%
-  summarise(n = n())
+
+
 species.n<- dat %>%
   group_by(species) %>%
   summarise(n = n())
 species.n
-for(i in 1:10) print(i)
-for(i in species){
-  p <- dat %>%
-    filter(species==i)%>%
-    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
-  print(p)
-}
+
+dat %>% 
+  count(species,specimen) %>% 
+  print() %>% 
+  count(species,name="n.specimens")
+
+
+#Code that shows PDF file for each species
 pdf("species_quadrant.pdf")
 for(i in species){
   p <- dat %>%
@@ -45,7 +53,5 @@ for(i in species){
   print(p)
 }
 dev.off()
+
 list.files(pattern=".pdf")
-
-
-
